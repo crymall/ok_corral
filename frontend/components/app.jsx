8174fddeafe4eecae9132bfefbf2de6b527bb189
Reed Gaines <reed.gaines@gmail.com>
2017-08-22 11:51:46 -0400

    implement app, root, finish login form

---
+++ frontend/components/app.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import LoginFormContainer from './session/login_form_container';
+
+const App = () => (
+  <div>
+    <LoginFormContainer />
+  </div>
+);
+
+export default App;
--- frontend/components/session/login_form.jsx
+++ frontend/components/session/login_form.jsx
@@ -5,6 +5,76 @@ class LoginForm extends React.Component {
 
   constructor(props) {
     super(props);
+
+    this.state = {
+      username: "",
+      password: ""
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleInput = this.handleInput.bind(this);
+    this.renderErrors = this.renderErrors.bind(this);
   }
-  
+
+  componentDidMount() {
+    this.props.clearErrors();
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.signedIn) {
+      this.props.history.push('/');
+    }
+  }
+
+  handleSubmit(e) {
+    e.preventDefault;
+    const user = {user: this.state};
+    this.props.processForm(user);
+  }
+
+  handleInput(type) {
+    return e => this.setState({
+      [type]: e.currentTarget.value;
+    });
+  }
+
+  renderErrors() {
+    if (this.props.errors.responseJSON) {
+      return (
+        <ul>
+          {this.props.errors.responseJSON.map((error, idx) => {
+            return (
+              <li key={`error-${idx}`}>
+                {error}
+              </li>
+            );
+          })}
+        </ul>
+      );
+    } else {
+      return null;
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        {this.renderErrors()}
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Username
+            <input name="user" onChange={this.handleInput('username')} value={`${this.state.username}`} />
+          </label>
+          <label>
+            Password
+            <input name="password" type="password" onChange={this.handleInput('password')} value={`${this.state.password}`} />
+          </label>
+          <input type="submit" value='Sign In' />
+        </form>
+      </div>
+    );
+  }
+
 }
+
+export default LoginForm
