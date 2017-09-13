a972469b2e1ee157a0ab18054eab5cc5a6aa8c9f
Reed Gaines <reed.gaines@gmail.com>
2017-09-13 12:11:30 -0400

    added production build

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,25 @@
-const path = require('path');
+var path = require('path');
+var webpack = require("webpack");
+
+var plugins = [];
+var devPlugins = [];
+
+var prodPlugins = [
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production')
+    }
+  }),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: true
+    }
+  })
+];
+
+plugins = plugins.concat(
+  process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
+)
 
 module.exports = {
   context: __dirname,
@@ -10,14 +31,7 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.jsx', '*']
   },
-  plugins: {
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify('production')
-      }
-    }),
-    new webpack.optimize.UglifyJsPlugin()
-  },
+  plugins: plugins,
   module: {
     loaders: [
       {
@@ -30,5 +44,5 @@ module.exports = {
       }
     ]
   },
-  devtool: 'source-maps'
+  devtool: 'source-map'
 };
